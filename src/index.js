90ac6705e50e1242eea54ca8aba2aa6474912ca5
Rodion Romanenko <rod.romanenko@gmail.com>
2021-07-13 10:34:01 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './sass/main.scss';
 import foodServiceMenu from './menu.json';
-// import foodMenuTemplate from './'
+import foodMenuTemplate from './menu-template.hbs';
+
+console.log(foodMenuTemplate(foodServiceMenu));
+
+const foodServiceMenuList = document.querySelector('.js-menu');
+foodServiceMenuList.innerHTML = foodMenuTemplate(foodServiceMenu);
