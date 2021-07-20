7b4620c5342e266a6315f6a66a4e691af1a772c6
Rodion Romanenko <rod.romanenko@gmail.com>
2021-07-20 14:13:53 +0400

    Update theme-switch.js

--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -6,15 +6,22 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-const currentTheme = localStorage.getItem('theme');
+refs.body.classList.add(
+    localStorage.getItem('theme') ? localStorage.getItem('theme') : Theme.LIGHT);
 
-if (currentTheme) {
-    document.documentElement.setAttribute('theme', currentTheme);
-    if (currentTheme === Theme.DARK) {
+if (refs.body.classList.value === Theme.DARK) {
     refs.checkboxSwitch.checked = true;
-    refs.body.classList.toggle(Theme.DARK);
-    }
 }
+    
+// const currentTheme = localStorage.getItem('theme');
+
+// if (currentTheme) {
+//     document.documentElement.setAttribute('theme', currentTheme);
+//     if (currentTheme === Theme.DARK) {
+//     refs.checkboxSwitch.checked = true;
+//     refs.body.classList.toggle(Theme.DARK);
+//     }
+// }
 
 // if (localStorage.getItem('theme') === Theme.DARK) {
 //     refs.body.classList.add(Theme.DARK);
@@ -31,31 +38,31 @@ const setLocalStorage = theme => {
     }
 };
 
-// const toggleClass = (addClass, removeClass) => {
-//     refs.body.classList.add(addClass);
-//     refs.body.classList.remove(removeClass);
+const toggleClass = (addClass, removeClass) => {
+    refs.body.classList.add(addClass);
+    refs.body.classList.remove(removeClass);
+}
+
+refs.checkboxSwitch.addEventListener('change', () => {
+
+    if (refs.checkboxSwitch.checked) {
+        toggleClass(Theme.DARK, Theme.LIGHT)
+        setLocalStorage(Theme.DARK);
+    } else {
+        toggleClass(Theme.LIGHT, Theme.DARK)
+        setLocalStorage(Theme.LIGHT);
+    };
+
+});
+
+// function switchTheme(event) {
+//     if (event.target.checked) {
+//     refs.body.classList.toggle(Theme.DARK);
+//     setLocalStorage(Theme.DARK);
+//     } else {
+//     refs.body.classList.toggle(Theme.DARK);
+//     setLocalStorage(Theme.LIGHT);  
+// }
 // }
 
-// refs.checkboxSwitch.addEventListener('change', () => {
-
-//     if (refs.checkboxSwitch.checked) {
-//         toggleClass(Theme.DARK, Theme.LIGHT)
-//         setLocalStorage(Theme.DARK);
-//     } else {
-//         toggleClass(Theme.LIGHT, Theme.DARK)
-//         setLocalStorage(Theme.LIGHT);
-//     };
-
-// });
-
-function switchTheme(event) {
-    if (event.target.checked) {
-    refs.body.classList.toggle(Theme.DARK);
-    setLocalStorage(Theme.DARK);
-    } else {
-    refs.body.classList.toggle(Theme.DARK);
-    setLocalStorage(Theme.LIGHT);  
-}
-}
-
-refs.checkboxSwitch.addEventListener('change', switchTheme, false);
+// refs.checkboxSwitch.addEventListener('change', switchTheme, false);
